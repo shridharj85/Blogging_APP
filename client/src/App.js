28cdf78d4f9019a492f5761263b6bfc1ed36984a
Shridhar jha <viratshridhar83@gmail.com>
2023-06-28 12:58:25 +0530

    home-page-buildup

---
+++ client/src/App.js
@@ -0,0 +1,79 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className = "logo">Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+
+        </nav>
+      </header>
+      <div className="post">
+      <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/05/GettyImages-1256069284.jpg?w=1390&crop=1"/> 
+
+        </div>
+        <div className="texts">
+
+        <h2>OpenAI’s ChatGPT app can now search the web — but only via Bing</h2>
+        <p className="info">
+          <a className="author">Shridhar</a>
+          <time>2023-06-28</time>
+        </p>
+        <p className="summary">Today, OpenAI announced that subscribers to ChatGPT Plus, the premium version of the company’s AI-powered chatbot, can use a new feature on the ChatGPT app called Browsing to have ChatGPT search Bing for answers to questions.</p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/05/GettyImages-1256069284.jpg?w=1390&crop=1"/> 
+
+        </div>
+        <div className="texts">
+
+        <h2>OpenAI’s ChatGPT app can now search the web — but only via Bing</h2>
+        <p className="info">
+          <a className="author">Shridhar</a>
+          <time>2023-06-28</time>
+        </p>
+        <p className="summary">Today, OpenAI announced that subscribers to ChatGPT Plus, the premium version of the company’s AI-powered chatbot, can use a new feature on the ChatGPT app called Browsing to have ChatGPT search Bing for answers to questions.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/05/GettyImages-1256069284.jpg?w=1390&crop=1"/> 
+
+        </div>
+        <div className="texts">
+
+        <h2>OpenAI’s ChatGPT app can now search the web — but only via Bing</h2>
+        <p className="info">
+          <a className="author">Shridhar</a>
+          <time>2023-06-28</time>
+        </p>
+        <p className="summary">Today, OpenAI announced that subscribers to ChatGPT Plus, the premium version of the company’s AI-powered chatbot, can use a new feature on the ChatGPT app called Browsing to have ChatGPT search Bing for answers to questions.</p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/05/GettyImages-1256069284.jpg?w=1390&crop=1"/> 
+
+        </div>
+        <div className="texts">
+
+        <h2>OpenAI’s ChatGPT app can now search the web — but only via Bing</h2>
+        <p className="info">
+          <a className="author">Shridhar</a>
+          <time>2023-06-28</time>
+        </p>
+        <p className="summary">Today, OpenAI announced that subscribers to ChatGPT Plus, the premium version of the company’s AI-powered chatbot, can use a new feature on the ChatGPT app called Browsing to have ChatGPT search Bing for answers to questions.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
